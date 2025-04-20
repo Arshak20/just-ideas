672b1d616dc3ff131e8c322002d830832ffc5fe6
Arshak Miqayelyan <arshak.miqayelyan@apekksu.com>
2025-04-20 21:28:37 +0400

    fix build error

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,8 +3,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/shared/query/query-client";
 import { FC, ReactNode } from "react";
-import MainLayout from "@/layouts/main/main";
-
 interface IProps {
   children: ReactNode;
 }
